25c1b8091159a05e20357cd8d6e50f1ec0992bf5
GeeNkonzo <gcobisankonzo@gmail.com>
2019-01-22 21:42:45 +0200

    bg color and basic styles added

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Todo list</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/styles.css" />
+    <link href="https://fonts.googleapis.com/css?family=Handlee|Mali" rel="stylesheet"> 
+    
+</head>
+<body>
+    <div class="container">
+        <div class="page">
+            <h1 class ="title">Todo List</h1>
+        </div>
+    </div>
+</body>
+</html>
